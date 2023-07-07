789a69c530e45b69e72acdfe957749e4816cb06c
Fio <leukert.fio@gmail.com>
2023-07-07 13:13:23 +0200

    add more domeobel1sk visuals

--- overwrites.gd
+++ overwrites.gd
@@ -4,6 +4,8 @@ const MOD_DOME_PATH := "res://mods-unpacked/Snek-Obel1sk/overwrites/content/dome
 const GAME_DOME_PATH := "res://content/dome/"
 const MOD_ICON_PATH := "res://mods-unpacked/Snek-Obel1sk/overwrites/content/icons/"
 const GAME_ICON_PATH := "res://content/icons/"
+const MOD_PETS_PATH := "res://mods-unpacked/Snek-Obel1sk/overwrites/content/pets/"
+const GAME_PETS_PATH := "res://content/pets/"
 
 var icons := [
 	"loadout_domeobel1sk.png",
@@ -26,6 +28,11 @@ func _init():
 	var primaryWeapon = "obel1sk"
 	var ow3 = load(str("res://content/weapons/" + primaryWeapon + "/" + startCaptialized(primaryWeapon) + ".tscn"))
 	ow3.take_over_path(str("res://content/weapons/" + primaryWeapon + "/" + primaryWeapon.capitalize() + ".tscn"))
+	
+	for i in range(6):
+		var id = i + 1
+		#var overwrite = load(str(MOD_PETS_PATH, "pet", id, "/PositionsDomeobel1sk.tscn"))
+		#overwrite.take_over_path(str(GAME_PETS_PATH, "pet", id, "/PositionsDomeobel1sk.tscn"))
 
 # so what the fuck is this? well .capitalize() also screws with numbers (i.e. obel1sk becomes Obel 1 sk, not Obel1sk.) Data would have this function but by the time this gets _init() called, Data doesn't exist yet, so this is the copy-pasted function straight from Data
 
